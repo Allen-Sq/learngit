d44d06e2a11a4f161e55c6b154c26727d5db2e6c
Allen Su <allen_su0611@126.com>
2017-06-26 15:51:24 +0800

    MvcMusciStore

---
+++ MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    /// <summary>
+    /// 负责网站根目录下的home page、about page和contact page
+    /// 每一个操作方法，都有一个同名的视图文件与其相对应，提供了视图与操作方法关联的基础。所以，可以直接利用隐式约定
+    /// 使用return view();渲染视图，并返回html页面
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 负责决定当浏览网站首页时触发的事件
+        /// 该方法渲染了Home Index视图
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View("about");//重写隐式约定中的视图选择逻辑，使首页显示其他页面
+        }
+
+        //使用ViewBag向视图传递少量数据
+        //控制器提供数据渲染视图
+        public ActionResult About()
+        {
+            ViewBag.Message = "我的第一个MVC程序！";
+            //利用隐式约定，定义了视图的选择逻辑，可以在/Viws/ControllerName目录下查找与操作方法名相同的视图文件
+            //不需要指定视图的文件名，直接return view()渲染视图
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //以下方法称为控制器操作，响应URL请求，执行正确的操作，并向浏览器做出响应
+        //该Index方法实现在页面上列出音乐商店里所有音乐流派的功能
+        // GET: /Store/
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        //控制器操作可以将查询字符串作为其操作方法的参数来接收
+        // GET:/Store/Browse?genre=?Disco
+        public string Browse(string genre)
+        {
+            //利用方法httputility.htmlencode来预处理用户输入，这样能阻止用户用链接向视图中注入js代码或html标记
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre  = " + genre);
+            return message ;
+        }
+
+        //读取和显示直接嵌入到URL中的输入参数
+        // GET:/Store/Details/5
+        public string Details(int id)
+        {
+            string message = "Store.Details, ID= " + id;
+            return message;
+        }
+	}
+}
